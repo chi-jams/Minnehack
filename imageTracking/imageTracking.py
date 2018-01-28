ee724c6bb3fe7d8ad811e05dd0ac9f26af8176ca
erbornemeier <erbornemeier@mines.edu>
2018-01-28 02:51:43 -0600

    updated to use cubemap

--- imageTracking/imageTracking.py
+++ imageTracking/imageTracking.py
@@ -1,13 +1,12 @@
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
 
 MIN_MATCH_COUNT = 10
 
-img1 = cv2.imread('pamphlet.jpg',0)          # queryImage
+img1 = cv2.imread('cubemap-colored.jpg',0)          # queryImage
 #img2 = cv2.imread('box_in_scene.png',0) # trainImage
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 while True:
     
@@ -17,11 +16,11 @@ while True:
     # Initiate SIFT detector
     #sift = cv2.SIFT()
     sift = cv2.xfeatures2d.SIFT_create()
-
+    
     # find the keypoints and descriptors with SIFT
     kp1, des1 = sift.detectAndCompute(img1,None)
     kp2, des2 = sift.detectAndCompute(img2,None)
-
+    
     FLANN_INDEX_KDTREE = 0
     index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
     search_params = dict(checks = 50)
@@ -45,9 +44,9 @@ while True:
 
         h,w = img1.shape
         pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-        dst = cv2.perspectiveTransform(pts,M)
-
-        img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+        if M is not None:
+            dst = cv2.perspectiveTransform(pts,M)
+            img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
 
     else:
         print( "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT))
